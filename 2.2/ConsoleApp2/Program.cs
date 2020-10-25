f2576b9cb06de7cd09aa809b1e36689ecf960d25
Finesta <59134396+Finesta@users.noreply.github.com>
2020-10-25 13:49:07 +0100

    Add files via upload

---
+++ 2.2/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            double delta;
+            Console.WriteLine("Podaj A");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj B");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj C");
+            c = int.Parse(Console.ReadLine());
+
+            delta = (b * b) - (4 * a * c);
+
+            Console.WriteLine(delta);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ 2.3/ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double waga, wzrost, bmi;
+
+            Console.WriteLine("Podaj wage w kilogramach");
+            waga = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj wzrost w metrach");
+            wzrost = double.Parse(Console.ReadLine());
+
+            bmi = waga / (wzrost * wzrost);
+
+            Console.WriteLine(Math.Round(bmi, 2));
+
+            Console.ReadKey();
+        }
+    }
+}
