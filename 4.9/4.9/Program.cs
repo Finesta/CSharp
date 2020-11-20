8c91e172de7314e21ed3b54e03f0d8391edcf848
Finesta <59134396+Finesta@users.noreply.github.com>
2020-11-20 21:28:20 +0100

    Add files via upload

---
+++ 4.9/4.9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace zad._4._09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ilosc = 0;
+            string tekst;
+            Console.WriteLine("Napisz zdanie: ");
+            tekst = Console.ReadLine();
+
+            if (tekst.Length > 0)
+            {
+                for (int i = 0; i < tekst.Length; i++)
+                {
+                    if (tekst[i] == ' ')
+                    {
+                        ilosc++;
+                    }
+                }
+                Console.WriteLine("Ilość wyrazów: {0}", ilosc + 1);
+            }
+            else
+            {
+                ilosc = 0;
+                Console.WriteLine("Ilość wyrazów: {0}", ilosc);
+            }
+        }
+    }
+}
