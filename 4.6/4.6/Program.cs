2fb1d1e0002d56523505f05757f675d14c7c5b11
Finesta <59134396+Finesta@users.noreply.github.com>
2020-11-20 21:26:56 +0100

    Add files via upload

---
+++ 4.6/4.6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _4._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] tablica2d = new int[5, 5] { { 1, 2, 1, 2, 1}, { 3, 4, 3, 4, 3 }, { 5, 6, 5, 6, 5 }, { 7, 8 , 7 , 8 ,7}, { 9, 0, 9, 0, 9} };
+            int sumprz = 0;
+
+            for (int y = 0; y < 5; y++)
+            {
+                for (int x = 0; x < 5; x++)
+                {
+                    Console.Write("{0,3}", tablica2d[x, y]);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int y = 0; y < 5; y++)
+            {
+                sumprz = sumprz + tablica2d[y,y];
+            }
+
+            Console.WriteLine("Suma przekatnej to {0}",sumprz);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 4.7/4.7/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _4._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] A = { { 1, 2, 3 }, { 4, 5, 6 } };
+            int[,] B = { { 7, 5, 6 }, { 0, 0, 0 } };
+
+            int y = 2;
+            int z = 3;
+            int[,] C = new int[y, z];
+
+            Console.WriteLine("Tablica 1\n");
+            int i;
+            int j = 0;
+            for (i = 0; i < 2; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    Console.Write("{0,3}", A[i, j]);
+                }
+                Console.Write("\n");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Tabblica 2\n");
+            int a;
+            int b = 0;
+            for (a = 0; a < 2; a++)
+            {
+                for (b = 0; b < 3; b++)
+                {
+                    Console.Write("{0,3}", B[a, b]);
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Zsumowane:\n");
+            int w, k;
+            for (w = 0; w < 2; w++)
+            {
+                for (k = 0; k < 3; k++)
+                {
+                    C[w, k] = A[w, k] + B[w, k];
+                    Console.Write("{0,3}", C[w, k]);
+                }
+                Console.Write("\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
