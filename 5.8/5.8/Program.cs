f4cbc7a5790824d05f6830a493574c6ad1ba8736
Finesta <59134396+Finesta@users.noreply.github.com>
2020-11-26 21:04:45 +0100

    Add files via upload

---
+++ 5.8/5.8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _5._8
+{
+    class Program
+    {
+        static void suma(string liczba)
+            {
+                int a = 0;
+
+                for (int i = 0; i < liczba.Length; i++)
+                   a = a + (Convert.ToInt16(liczba[i].ToString())); 
+
+                Console.WriteLine("Suma cyfr liczby {0} wynosi {1}", liczba, a);
+            }
+
+        static void Main(string[] args)
+            {
+                Console.WriteLine("Podaj liczbę całkowitą.");
+                string liczba = Console.ReadLine();
+                suma(liczba);
+            }
+    }
+}
---
+++ 5.9/5.9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace zad._5._09
+{
+    class Program
+    {
+
+        static int it(int n)
+        {
+            int a = 0;
+            int b = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                b += a;
+                a = b - a;
+            }
+            return a;
+        }
+
+        static int r(int n)
+        {
+            if ((n == 1) || (n == 2))
+                return 1;
+            else
+                return r(n - 1) + r(n - 2);
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("Podaj dlugosc ciagu");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} wyraz ciagu (rekurencja): {1}", n, r(n));
+            Console.WriteLine("{0} wyraz ciagu (iteracja): {1}", n, it(n));
+        }
+    }
+
+}
